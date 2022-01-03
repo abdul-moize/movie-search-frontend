1b96a69ba4b0c51d0a01cb569b8d498f6a85df73
mhumayun <abdul.moize@arbisoft.com>
2022-01-03 18:47:55 +0500

    feat: material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import styled from '@emotion/styled';
 import { React } from 'react';
 import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import store from './redux/store';
 
+const MainContainer = styled.body`
+  background-image: url('/movie-bobbin-popcorn-basket.jpg');
+  background-position: center;
+  background-size: cover;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  
+`;
 function App() {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route exact path="/" element={<LoginPage />} />
-      </Routes>
+      <MainContainer>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+        </Routes>
+      </MainContainer>
     </Provider>
   );
 }
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,50 +1,91 @@
-import { React, useRef } from 'react';
+import styled from '@emotion/styled';
+import { Button, TextField, Typography } from '@mui/material';
+import { React, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setToken } from '../../redux/nodes/entities/user/actions';
 import login from '../../services/authService';
-import styles from './loginPage.module.css';
+
+const StyledButton = styled(Button, {})`
+  font-size: larger;
+  margin: 10px;
+`;
+
+const ErrorText = styled(Typography)`
+  margin: 10px;
+`;
+
+const Container = styled.div`
+  width: 30%;
+  height: 50%;
+  display: flex;
+  margin: auto;
+  margin-top: 100px;
+  align-items: center;
+  flex-direction: column;
+  background: white;
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  margin-top: 60px;
+  width: 80%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FormTextField = styled(TextField)`
+  width: 80%;
+`;
 
 function LoginPage() {
   const formRef = useRef();
 
+  const [errorMessage, setErrorMessage] = useState('');
   const dis = useDispatch();
   const nav = useNavigate();
   const onSubmit = async (e) => {
     e.preventDefault();
+    setErrorMessage('');
     const formData = new FormData(formRef.current);
     const { token, refreshToken } = await login(formData);
-    dis(setToken(token, refreshToken));
-    nav('/home', { replace: true });
+    if (token && refreshToken) {
+      dis(setToken(token, refreshToken));
+      nav('/home', { replace: true });
+    } else {
+      setErrorMessage('Wrong Email or Password');
+    }
   };
 
   return (
-    <div className={styles['main-box']}>
-      <h1>Login Page</h1>
-      <form onSubmit={onSubmit} ref={formRef} className={styles['form-box']}>
-        <label className={styles.label} htmlFor="email">
-          <h3 className={styles['label-heading']}>Email</h3>
-          <input
-            name="email"
-            className={styles['input-field']}
-            type="email"
-            placeholder="Enter email"
-          />
-        </label>
-        <label className={styles.label} htmlFor="password">
-          <h3 className={styles['label-heading']}>Password</h3>
-          <input
-            className={styles['input-field']}
-            type="password"
-            name="password"
-            placeholder="Enter password"
-          />
-        </label>
-        <button className={styles['submit-btn']} type="submit">
+    <Container>
+      <Typography variant="h3">Login Page</Typography>
+      <FormContainer onSubmit={onSubmit} ref={formRef}>
+        {errorMessage && <ErrorText color="error">{errorMessage}</ErrorText>}
+        <FormTextField
+          variant="outlined"
+          label="Email"
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          required
+          margin="dense"
+        />
+        <FormTextField
+          variant="outlined"
+          label="Password"
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          required
+          margin="dense"
+        />
+        <StyledButton variant="contained" type="submit">
           Log in
-        </button>
-      </form>
-    </div>
+        </StyledButton>
+      </FormContainer>
+    </Container>
   );
 }
 
